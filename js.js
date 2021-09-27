77d8f2524915bf0dabdee11c0ecb7b62d019c440
Aine Mota <motaaine1@gmail.com>
2021-09-27 16:51:57 -0300

    cir o arquivo css e iniciei a função de inserir as torres

---
+++ js.js
@@ -0,0 +1,10 @@
+const main = document.getElementsByTagName('main');
+
+
+let divCreate = () =>{
+    const div = document.createElement('div');
+console.log(div)
+    main.appendChild(div)
+}
+
+divCreate();
