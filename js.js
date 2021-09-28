8e7199ab0b45585c160d7e9575065e68e53c6eeb
Otávio Takashi Fuzeto Ikegami <otavinhoikgi@gmail.com>
2021-09-27 21:23:55 -0300

    funcao versao 1

---
+++ js.js
@@ -0,0 +1,38 @@
+function movimentoDisco (){
+    let cliques=0;
+    let moves=0;
+    let torreResultado=document.getElementsById('torre2')
+    let torres = document.getElementById('torres#colcarid#').click(function(){
+        cliques++;
+        if(cliques===1){primeiraTorre}
+        else if(cliques===2){segundaTorre}
+    })
+    if (torreDisco.childElementCount===0 // || se o alvo for menor 
+        ){alert('Escolha inválida!')}
+    else{
+        let disco =torreDisco.lastChild;
+        torreAlvo.appendChild(disco);
+        moves++;
+    }
+    if(torre.childElementCount===5){
+        //ganhou
+    }
+}
+
+
+
+
+//torre0 torre1 torre2
+
+function primeiraTorre (){
+    let torres = document.getElementById('torres#colocarid#');
+    torres.addEventListener('click', function(x) {
+    let torreDisco = document.getElementById(x.target.id);
+    return torreDisco;
+})}
+function segundaTorre (){
+    let torres = document.getElementById('torres#colocarid#');
+    torres.addEventListener('click', function(y) {
+    let torreAlvo = document.getElementById(y.target.id);
+    return torreAlvo;
+})}
